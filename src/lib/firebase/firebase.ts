d769aa02ea8d5ac98899a3ebb1e16b20f6c51307
yoon3n4m <lgmysn2@gmail.com>
2024-10-26 11:09:20 +0900

    feat: firebase

---
+++ src/lib/firebase/firebase.ts
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { addDoc, collection, getDocs, getFirestore, query } from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const dbService = getFirestore(app);
+export const dbAddDoc = addDoc;
+export const dbCollection = collection;
+export const dbGetDocs = getDocs;
+export const dbQuery = query;
+export const storageService = getStorage();
