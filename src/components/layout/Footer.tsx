b55b87a17fba4dbb01937175485ccb389c4772e7
yoon3n4m <lgmysn2@gmail.com>
2024-10-27 23:07:35 +0900

    fix: metadata

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,7 +9,7 @@ function Footer(props: FooterProps) {
     <div className="py-xxxl mt-auto text-center opacity-70 text-xs">
       <p className="">hanroro panpage</p>
       <a href="https://open.kakao.com/o/s6CV3FWg" target="_blank" rel="noopener noreferrer">
-        contact
+        &gt; contact kakaotalk &lt;
       </a>
     </div>
   );
