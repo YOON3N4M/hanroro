035443b2875edf804465843266a58a40bc56fa7a
윤세남 <lgmysn2@gmail.com>
2024-10-28 10:50:38 +0900

    fix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,32 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
-import ModalPortal from "@/components/modal/ModalPortal";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
+import ModalPortal from '@/components/modal/ModalPortal'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "한로로 팬페이지",
-  description: "한로로 팬페이지",
-};
+	title: 'hanroro | panpage',
+	description: '한로로 팬페이지',
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html className="h-full" lang="en">
-      <body className="h-full w-full text-authentic-dark">
-        <Header />
-        <main className="inner h-full bg-white flex flex-col">
-          {children}
-          <Footer />
-        </main>
-        <ModalPortal />
-      </body>
-    </html>
-  );
+	return (
+		<html className="h-full" lang="en">
+			<body className="h-full w-full text-authentic-dark">
+				<Header />
+				<main className="inner h-full bg-white flex flex-col">
+					{children}
+					<Footer />
+				</main>
+				<ModalPortal />
+			</body>
+		</html>
+	)
 }
