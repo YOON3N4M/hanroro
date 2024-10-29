11b6f19187949ce04030756d3e632c295c75bf46
윤세남 <lgmysn2@gmail.com>
2024-10-29 12:59:53 +0900

    chore: book data

---
+++ src/data/book.ts
@@ -0,0 +1,35 @@
+export interface Book {
+	id: number
+	title: string
+	author: string
+	url: string
+}
+
+export const RECOMMEND_BOOK_LIST: Book[] = [
+	{ id: 0, title: '불온한 검은 피', author: '허연', url: '' },
+	{ id: 1, title: '자면서도 다 듣는 애인아', author: '김개미', url: '' },
+	{ id: 2, title: '손을 잡으면 눈이 녹아', author: '장수양', url: '' },
+	{ id: 3, title: '못 속에는 못 속이는 이야기', author: '김박은경', url: '' },
+	{ id: 4, title: '그리하여 흘려 쓴 것들', author: '이제니', url: '' },
+	{
+		id: 5,
+		title: '언니의 나라에선 누구도 시들지 않기 때문,',
+		author: '김희준',
+		url: '',
+	},
+	{ id: 6, title: '사랑을 위한 되풀이', author: '황인찬', url: '' },
+	{ id: 7, title: '랑과 나의 사막', author: '천선란', url: '' },
+	{ id: 8, title: '소년이 온다', author: '한강', url: '' },
+	{ id: 9, title: '숲에서 한나절', author: '남영화', url: '' },
+	{ id: 10, title: '사랑의 기술', author: '에리히 프롬', url: '' },
+	{ id: 11, title: '고통을 말하지 않는 법', author: '마리아 투마킨', url: '' },
+	{ id: 12, title: '버섯 농장', author: '성혜령', url: '' },
+	{ id: 13, title: '김청귤', author: '재와 물거품', url: '' },
+	{ id: 14, title: '구의 증명', author: '최진영', url: '' },
+	{ id: 15, title: '배예람', author: '좀비즈 어웨이', url: '' },
+	{ id: 16, title: '망각하는 자에게 축복을', author: '민지형', url: '' },
+	{ id: 17, title: '아름다웠던 사람의 이름은 혼자', author: '이현호', url: '' },
+	{ id: 18, title: '머지않아 여름은 끝나겠지만', author: '김종완', url: '' },
+	{ id: 19, title: '리틀 라이프', author: '한야 야나기하라', url: '' },
+	{ id: 20, title: '숲의 소실점을 향해', author: '양안다', url: '' },
+]
