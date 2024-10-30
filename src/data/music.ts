b08bbefeb1db381547ce50ce86a76e88c7e06b37
윤세남 <lgmysn2@gmail.com>
2024-10-30 12:38:19 +0900

    test: deploy

--- src/data/music.ts
+++ src/data/music.ts
@@ -1,51 +1,53 @@
 export interface Music {
-  id: number;
-  name: string;
-  artist: string;
-  url: string;
+	id: number
+	name: string
+	artist: string
+	url: string
 }
 
 export const RECOMMEND_MUSIC_LIST: Music[] = [
-  {
-    id: 0,
-    name: "new you",
-    artist: "my bloody valentine",
-    url: "https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm",
-  },
-  {
-    id: 1,
-    name: "AA BOUQUET FOR YOUR 180 FACE",
-    artist: "Saya Gray",
-    url: "https://youtu.be/Uzn9aipflN8?si=2e9GUjDesjs21adv",
-  },
-  {
-    id: 2,
-    name: "new you",
-    artist: "my bloody valentine",
-    url: "https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm",
-  },
-  {
-    id: 3,
-    name: "new you",
-    artist: "my bloody valentine",
-    url: "https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm",
-  },
-  {
-    id: 4,
-    name: "new you",
-    artist: "my bloody valentine",
-    url: "https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm",
-  },
-  {
-    id: 5,
-    name: "new you",
-    artist: "my bloody valentine",
-    url: "https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm",
-  },
-  //   {
-  //     id: 1,
-  //     name: "new you",
-  //     artist: "my bloody valentine",
-  //     url: "https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm",
-  //   },
-];
+	{
+		id: 0,
+		name: 'new you',
+		artist: 'my bloody valentine',
+		url: 'https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm',
+	},
+	{
+		id: 1,
+		name: 'AA BOUQUET FOR YOUR 180 FACE',
+		artist: 'Saya Gray',
+		url: 'https://youtu.be/Uzn9aipflN8?si=2e9GUjDesjs21adv',
+	},
+	{
+		id: 2,
+		name: 'new you',
+		artist: 'my bloody valentine',
+		url: 'https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm',
+	},
+	{
+		id: 3,
+		name: 'new you',
+		artist: 'my bloody valentine',
+		url: 'https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm',
+	},
+	{
+		id: 4,
+		name: 'new you',
+		artist: 'my bloody valentine',
+		url: 'https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm',
+	},
+	{
+		id: 5,
+		name: 'new you',
+		artist: 'my bloody valentine',
+		url: 'https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm',
+	},
+	//   {
+	//     id: 1,
+	//     name: "new you",
+	//     artist: "my bloody valentine",
+	//     url: "https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm",
+	//   },
+]
+
+const test = ''
