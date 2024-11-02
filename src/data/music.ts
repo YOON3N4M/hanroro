e915094379c89b28aad7639ab3137e135d7bd551
yoon3nam <lgmysn2@gmail.com>
2024-11-02 15:39:53 +0900

    chore: music data

--- src/data/music.ts
+++ src/data/music.ts
@@ -1,51 +1,40 @@
 export interface Music {
-	id: number
-	name: string
-	artist: string
-	url: string
+  id: number;
+  name: string;
+  artist: string;
+  url: string;
 }
 
 export const RECOMMEND_MUSIC_LIST: Music[] = [
-	{
-		id: 0,
-		name: 'new you',
-		artist: 'my bloody valentine',
-		url: 'https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm',
-	},
-	{
-		id: 1,
-		name: 'AA BOUQUET FOR YOUR 180 FACE',
-		artist: 'Saya Gray',
-		url: 'https://youtu.be/Uzn9aipflN8?si=2e9GUjDesjs21adv',
-	},
-	{
-		id: 2,
-		name: 'new you',
-		artist: 'my bloody valentine',
-		url: 'https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm',
-	},
-	{
-		id: 3,
-		name: 'new you',
-		artist: 'my bloody valentine',
-		url: 'https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm',
-	},
-	{
-		id: 4,
-		name: 'new you',
-		artist: 'my bloody valentine',
-		url: 'https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm',
-	},
-	{
-		id: 5,
-		name: 'new you',
-		artist: 'my bloody valentine',
-		url: 'https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm',
-	},
-	//   {
-	//     id: 1,
-	//     name: "new you",
-	//     artist: "my bloody valentine",
-	//     url: "https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm",
-	//   },
-]
+  {
+    id: 0,
+    name: "AA BOUQUET FOR YOUR 180 FACE",
+    artist: "Saya Gray",
+    url: "https://youtu.be/Uzn9aipflN8?si=2e9GUjDesjs21adv",
+  },
+  {
+    id: 1,
+    name: "I Knew",
+    artist: "Lizzy McAlpine",
+    url: "https://youtu.be/D7ICDq2o-vw?si=8N3USbs3prXje4NK",
+  },
+  {
+    id: 2,
+    name: "new you",
+    artist: "my bloody valentine",
+    url: "https://youtu.be/VpoOjoiYcWY?si=2i1ekpYOUSkyB5Lm",
+  },
+
+  {
+    id: 3,
+    name: "Hachiko",
+    artist: "BIG Naughty, Sion, Yescoba, Dayoung Ahn",
+    url: "https://youtu.be/JXYZnLNEndA?si=h-iKkBUcZ7CaO2Vg",
+  },
+  {
+    id: 4,
+    name: "작은 것들을 위한 시 (Boy With Luv)",
+    artist: "BTS",
+    url: "https://youtu.be/XsX3ATc3FbA?si=nXxR2Ndno_MIk_Rb",
+  },
+];
