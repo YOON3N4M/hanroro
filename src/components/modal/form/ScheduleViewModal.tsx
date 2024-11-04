b650a925cb18ab03317356771ec661d9a502ada2
yoon3n4m <lgmysn2@gmail.com>
2024-11-04 22:47:43 +0900

    feat: ScheduleViewModal component

--- src/components/modal/form/ScheduleViewModal.tsx
+++ src/components/modal/form/ScheduleViewModal.tsx
@@ -1,6 +1,63 @@
-import React from 'react'
-import ModalTemplate from '../ModalTemplate'
+import React from "react";
+import ModalTemplate from "../ModalTemplate";
+import { Schedule } from "@/data/schedule";
+import { cn } from "@/utils";
+import { scheduleTypeColorStyles } from "@/components/calendar";
+import NewTabAnchor from "@/components/ui/NewTabAnchor";
+import Image from "next/image";
+import { IconCalendar, IconLink, IconLocation, IconTimer } from "@/components/svg";
 
-export default function ScheduleViewModal() {
-	return <ModalTemplate>scheduleViewModal</ModalTemplate>
+interface ScheduleViewModalProps {
+  schedule: Schedule;
+}
+
+export default function ScheduleViewModal(props: ScheduleViewModalProps) {
+  const { schedule } = props;
+  const { type, title, date, duration, location, desc, link, images } = schedule;
+  return (
+    <ModalTemplate>
+      <div className="min-w-[500px]">
+        <div className="flex items-center text-sm gap-xxs">
+          <span className={cn("size-[5px] rounded-full", scheduleTypeColorStyles[type].default)}></span>
+          <span>{type}</span>
+        </div>
+        <div>
+          <h2>{title}</h2>
+        </div>
+        <div className="flex justify-center mt-sm">
+          {images && (
+            <Image
+              src={images[0].src}
+              width={images[0].width}
+              height={images[0].height}
+              className="max-h-[500px] w-auto"
+              alt={title}
+            />
+          )}
+        </div>
+        <div className="mt-sm flex flex-col gap-xxs">
+          <div className="flex gap-sm text-sm">
+            <div className="flex gap-xxs items-center">
+              <IconCalendar />
+              {date}
+            </div>
+            <span className="flex gap-xxs items-center">
+              <IconTimer />
+              {duration}
+            </span>
+          </div>
+          <div className="text-sm flex gap-xxs items-center">
+            <IconLocation />
+            {location}
+          </div>
+          <div className="flex gap-xxs items-center">
+            <IconLink />
+            <NewTabAnchor className="text-sm underline" href={link}>
+              {link}
+            </NewTabAnchor>
+          </div>
+        </div>
+      </div>
+    </ModalTemplate>
+  );
 }
--- src/components/svg/index.ts
+++ src/components/svg/index.ts
@@ -5,3 +5,7 @@ export { AiOutlineSpotify as IconSpotify } from "react-icons/ai";
 
 export { BiSolidLeftArrow as IconRightLeft } from "react-icons/bi";
 export { BiSolidRightArrow as IconRightRight } from "react-icons/bi";
+export { CiCalendar as IconCalendar } from "react-icons/ci";
+export { CiTimer as IconTimer } from "react-icons/ci";
+export { CiLink as IconLink } from "react-icons/ci";
+export { CiLocationOn as IconLocation } from "react-icons/ci";
