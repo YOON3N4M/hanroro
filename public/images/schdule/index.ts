03ad2fc2e3b4c27df8b063cc61d158e771c3776b
윤세남 <lgmysn2@gmail.com>
2024-11-06 15:14:08 +0900

    chore: schedule 데이터 추가

--- public/images/schdule/index.ts
+++ public/images/schdule/index.ts
@@ -1,1 +1,4 @@
-export { default as wonderLivet } from "./wonderlivet/wonder-livet-2024.jpg";
+export { default as wonderLivet } from './wonderlivet/wonder-livet-2024.jpg'
+export { default as birthdayCafe1 } from './birthday/1.jpg'
+export { default as birthdayCafe2 } from './birthday/2.jpg'
+export { default as birthdayCafe3 } from './birthday/3.jpg'
--- src/data/schedule.ts
+++ src/data/schedule.ts
@@ -1,37 +1,54 @@
-import { StaticImageData } from "next/image";
-import { wonderLivet } from "../../public/images/schdule";
+import { StaticImageData } from 'next/image'
+import {
+	birthdayCafe1,
+	birthdayCafe2,
+	birthdayCafe3,
+	wonderLivet,
+} from '../../public/images/schdule'
 
-export type ScheduleType = "concert" | "anniversary" | "release" | "event" | "etc";
+export type ScheduleType =
+	| 'concert'
+	| 'anniversary'
+	| 'release'
+	| 'event'
+	| 'etc'
 
 export interface Schedule {
-  type: ScheduleType;
-  title: string;
-  //date - yyyy-MM-dd
-  date: string[];
-  duration: string;
-  location: string;
-  desc?: string;
-  link?: string;
-  images?: StaticImageData[];
+	type: ScheduleType
+	title: string
+	//date - yyyy-MM-dd
+	date: string[]
+	duration: string
+	location: string
+	desc?: string
+	link?: string
+	images?: StaticImageData[]
 }
 
 export const SCHEDULE_LIST: Schedule[] = [
-  {
-    type: "concert",
-    title: "WONDERLIVET 2024",
-    date: ["2024-11-08"],
-    duration: "14:50~15:30",
-    location: "일산 킨텍스 원더스테이지",
-    link: "https://wonderli.vet/",
-    images: [wonderLivet],
-  },
-  {
-    type: "event",
-    title: "한로로 생일카페",
-    date: ["2024-11-09", "2024-11-10"],
-    duration: "00:00~00:00",
-    location: "일산 킨텍스 원더스테이지",
-    link: "https://wonderli.vet/",
-    images: [wonderLivet],
-  },
-];
+	{
+		type: 'concert',
+		title: 'WONDERLIVET 2024',
+		date: ['2024-11-08'],
+		duration: '14:50~15:30',
+		location: '일산 킨텍스 원더스테이지',
+		link: 'https://wonderli.vet/',
+		images: [wonderLivet],
+	},
+	{
+		type: 'event',
+		title: '한로로 생일카페',
+		date: ['2024-11-09', '2024-11-10'],
+		duration: '12:00~20:00 (19:30 last order)',
+		location: 'Waving (서울 광진구 광나루로 16길 5)',
+		link: 'https://www.instagram.com/p/DBkk1JzSG18/?igsh=MXBiYWpxazJoNjJ3OQ==',
+		images: [birthdayCafe1, birthdayCafe2, birthdayCafe3],
+	},
+	{
+		type: 'anniversary',
+		title: '한로로 데뷔 D+1000',
+		date: ['2024-12-07'],
+		duration: '-',
+		location: '-',
+	},
+]
