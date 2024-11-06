0f063215b17d518d268375adaa4426dd6d97b590
윤세남 <lgmysn2@gmail.com>
2024-11-06 15:21:05 +0900

    fix: mo ScheduleViewModal

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,100 +1,115 @@
-"use client";
+'use client'
 
-import { format, startOfMonth } from "date-fns";
-import useCalendar from "./useCalendar";
-import { IconRightLeft, IconRightRight } from "../svg";
-import { cn } from "@/utils";
-import { Schedule, SCHEDULE_LIST, ScheduleType } from "@/data/schedule";
-import useModal from "../modal/useModal";
-import ScheduleViewModal from "../modal/form/ScheduleViewModal";
+import { format, startOfMonth } from 'date-fns'
+import useCalendar from './useCalendar'
+import { IconRightLeft, IconRightRight } from '../svg'
+import { cn } from '@/utils'
+import { Schedule, SCHEDULE_LIST, ScheduleType } from '@/data/schedule'
+import useModal from '../modal/useModal'
+import ScheduleViewModal from '../modal/form/ScheduleViewModal'
 
-const weeks = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+const weeks = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
 export default function Calendar() {
-  const { currentDate, daysOfMonth, nextMonth, prevMonth } = useCalendar();
+	const { currentDate, daysOfMonth, nextMonth, prevMonth } = useCalendar()
 
-  return (
-    <div className="text-xs">
-      {/* controller */}
-      <div className="flex justify-between">
-        <div className="flex min-w-[80px] justify-center">
-          <span>
-            {format(currentDate, "yyyy")}.{format(currentDate, "MM")}
-          </span>
-        </div>
-        <div className="flex min-w-[80px] justify-center">
-          <button onClick={prevMonth}>
-            <IconRightLeft />
-          </button>
-          <button onClick={nextMonth}>
-            <IconRightRight />
-          </button>
-        </div>
-      </div>
-      {/* MTWTFSS */}
-      <div className="grid grid-cols-7 mt-xs">
-        {weeks.map((week, idx) => (
-          <div key={week} className="flex justify-center">
-            <span className={cn(idx === weeks.length - 1 && "text-red-300")}>{week}</span>
-          </div>
-        ))}
-      </div>
-      {/* days */}
-      <div className="grid grid-cols-7 mt-xs">
-        {daysOfMonth.map((day, idx) => (
-          <DayGrid key={`${format(day, "yyyy-MM-dd")}-${idx}`} day={day} currentDate={currentDate} />
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="text-xs">
+			{/* controller */}
+			<div className="flex justify-between">
+				<div className="flex min-w-[80px] justify-center">
+					<span>
+						{format(currentDate, 'yyyy')}.{format(currentDate, 'MM')}
+					</span>
+				</div>
+				<div className="flex min-w-[80px] justify-center">
+					<button onClick={prevMonth}>
+						<IconRightLeft />
+					</button>
+					<button onClick={nextMonth}>
+						<IconRightRight />
+					</button>
+				</div>
+			</div>
+			{/* MTWTFSS */}
+			<div className="grid grid-cols-7 mt-xs">
+				{weeks.map((week, idx) => (
+					<div key={week} className="flex justify-center">
+						<span className={cn(idx === weeks.length - 1 && 'text-red-300')}>
+							{week}
+						</span>
+					</div>
+				))}
+			</div>
+			{/* days */}
+			<div className="grid grid-cols-7 mt-xs">
+				{daysOfMonth.map((day, idx) => (
+					<DayGrid
+						key={`${format(day, 'yyyy-MM-dd')}-${idx}`}
+						day={day}
+						currentDate={currentDate}
+					/>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export const scheduleTypeColorStyles: {
-  [key in ScheduleType]: { default: string; hover: string };
+	[key in ScheduleType]: { default: string; hover: string }
 } = {
-  concert: { default: "bg-blue-300", hover: "hover:bg-blue-400" },
-  event: { default: "bg-yellow-200", hover: "hover:bg-yellow-400" },
-  anniversary: { default: "bg-blue-300", hover: "bg-blue-400" },
-  release: { default: "bg-blue-300", hover: "bg-blue-400" },
-  etc: { default: "bg-blue-300", hover: "bg-blue-400" },
-};
+	concert: { default: 'bg-blue-300', hover: 'hover:bg-blue-400' },
+	event: { default: 'bg-yellow-200', hover: 'hover:bg-yellow-400' },
+	anniversary: { default: 'bg-red-300', hover: 'hover:bg-red-400' },
+	release: { default: 'bg-blue-300', hover: 'bg-blue-400' },
+	etc: { default: 'bg-blue-300', hover: 'bg-blue-400' },
+}
 
 function DayGrid({ currentDate, day }: { currentDate: Date; day: Date }) {
-  const formattedDay = format(day, "yyyy-MM-dd");
-  const isSunday = format(day, "iii") === "Sun";
-  const isDayOfCurrentDate = format(day, "LLL") === format(currentDate, "LLL");
+	const formattedDay = format(day, 'yyyy-MM-dd')
+	const isSunday = format(day, 'iii') === 'Sun'
+	const isDayOfCurrentDate = format(day, 'LLL') === format(currentDate, 'LLL')
 
-  const scheduleListOfDay = SCHEDULE_LIST.filter((schedule) => schedule.date.includes(formattedDay));
-  console.log(scheduleListOfDay);
-  return (
-    <div className="min-h-[50px]">
-      <div className="flex justify-center">
-        <span className={cn(isSunday && "text-red-300", !isDayOfCurrentDate && "opacity-40")}>{format(day, "dd")}</span>
-      </div>
-      <div className="flex flex-col items-center justify-center mt-xxxs">
-        {scheduleListOfDay.map((schedule) => (
-          <ScheduleItem key={schedule.title} schedule={schedule} />
-        ))}
-      </div>
-    </div>
-  );
+	const scheduleListOfDay = SCHEDULE_LIST.filter((schedule) =>
+		schedule.date.includes(formattedDay),
+	)
+	console.log(scheduleListOfDay)
+	return (
+		<div className="min-h-[50px]">
+			<div className="flex justify-center">
+				<span
+					className={cn(
+						isSunday && 'text-red-300',
+						!isDayOfCurrentDate && 'opacity-40',
+					)}
+				>
+					{format(day, 'dd')}
+				</span>
+			</div>
+			<div className="flex flex-col items-center justify-center mt-xxxs">
+				{scheduleListOfDay.map((schedule) => (
+					<ScheduleItem key={schedule.title} schedule={schedule} />
+				))}
+			</div>
+		</div>
+	)
 }
 
 function ScheduleItem({ schedule }: { schedule: Schedule }) {
-  const { openSingleModal } = useModal();
+	const { openSingleModal } = useModal()
 
-  function onClickSchedule() {
-    openSingleModal(<ScheduleViewModal schedule={schedule} />);
-  }
-  return (
-    <button
-      onClick={onClickSchedule}
-      key={schedule.title}
-      className={cn(
-        "w-[80%] min-h-[5px] rounded-md",
-        scheduleTypeColorStyles[schedule.type].default,
-        scheduleTypeColorStyles[schedule.type].hover
-      )}
-    ></button>
-  );
+	function onClickSchedule() {
+		openSingleModal(<ScheduleViewModal schedule={schedule} />)
+	}
+	return (
+		<button
+			onClick={onClickSchedule}
+			key={schedule.title}
+			className={cn(
+				'w-[80%] min-h-[5px] rounded-md',
+				scheduleTypeColorStyles[schedule.type].default,
+				scheduleTypeColorStyles[schedule.type].hover,
+			)}
+		></button>
+	)
 }
--- src/components/modal/form/ScheduleViewModal.tsx
+++ src/components/modal/form/ScheduleViewModal.tsx
@@ -1,63 +1,89 @@
-import React from "react";
-import ModalTemplate from "../ModalTemplate";
-import { Schedule } from "@/data/schedule";
-import { cn } from "@/utils";
-import { scheduleTypeColorStyles } from "@/components/calendar";
-import NewTabAnchor from "@/components/ui/NewTabAnchor";
-import Image from "next/image";
-import { IconCalendar, IconLink, IconLocation, IconTimer } from "@/components/svg";
+import React from 'react'
+import ModalTemplate from '../ModalTemplate'
+import { Schedule } from '@/data/schedule'
+import { cn } from '@/utils'
+import { scheduleTypeColorStyles } from '@/components/calendar'
+import NewTabAnchor from '@/components/ui/NewTabAnchor'
+import Image from 'next/image'
+import {
+	IconCalendar,
+	IconLink,
+	IconLocation,
+	IconTimer,
+} from '@/components/svg'
 
 interface ScheduleViewModalProps {
-  schedule: Schedule;
+	schedule: Schedule
 }
 
 export default function ScheduleViewModal(props: ScheduleViewModalProps) {
-  const { schedule } = props;
-  const { type, title, date, duration, location, desc, link, images } = schedule;
-  return (
-    <ModalTemplate>
-      <div className="min-w-[500px]">
-        <div className="flex items-center text-sm gap-xxs">
-          <span className={cn("size-[5px] rounded-full", scheduleTypeColorStyles[type].default)}></span>
-          <span>{type}</span>
-        </div>
-        <div>
-          <h2>{title}</h2>
-        </div>
-        <div className="flex justify-center mt-sm">
-          {images && (
-            <Image
-              src={images[0].src}
-              width={images[0].width}
-              height={images[0].height}
-              className="max-h-[500px] w-auto"
-              alt={title}
-            />
-          )}
-        </div>
-        <div className="mt-sm flex flex-col gap-xxs">
-          <div className="flex gap-sm text-sm">
-            <div className="flex gap-xxs items-center">
-              <IconCalendar />
-              {date}
-            </div>
-            <span className="flex gap-xxs items-center">
-              <IconTimer />
-              {duration}
-            </span>
-          </div>
-          <div className="text-sm flex gap-xxs items-center">
-            <IconLocation />
-            {location}
-          </div>
-          <div className="flex gap-xxs items-center">
-            <IconLink />
-            <NewTabAnchor className="text-sm underline" href={link}>
-              {link}
-            </NewTabAnchor>
-          </div>
-        </div>
-      </div>
-    </ModalTemplate>
-  );
+	const { schedule } = props
+	const { type, title, date, duration, location, desc, link, images } = schedule
+	return (
+		<ModalTemplate>
+			<div className="min-w-[500px] mo:min-w-[80vw] mo:max-w-[80vw]  ">
+				<div className="flex items-center text-sm gap-xxs">
+					<span
+						className={cn(
+							'size-[5px] rounded-full',
+							scheduleTypeColorStyles[type].default,
+						)}
+					></span>
+					<span>{type}</span>
+				</div>
+				<div>
+					<h2>{title}</h2>
+				</div>
+				<div className="flex justify-center mt-sm">
+					{images && (
+						<Image
+							src={images[0].src}
+							width={images[0].width}
+							height={images[0].height}
+							className="max-h-[500px] w-auto"
+							alt={title}
+						/>
+					)}
+				</div>
+				<div className="mt-sm flex flex-col gap-xxs">
+					<div className="flex pc:gap-sm mo:gap-xxs text-sm mo:flex-col">
+						<div className="flex gap-xxs items-center">
+							<IconCalendar />
+							<div className="flex gap-xxxs">
+								{date.map((d, idx) => (
+									<span>
+										{idx !== 0 ? '- ' : ''}
+										{d}
+									</span>
+								))}
+							</div>
+						</div>
+						<span className="flex gap-xxs items-center">
+							<IconTimer />
+							{duration}
+						</span>
+					</div>
+					<div className="text-sm flex gap-xxs items-center">
+						<IconLocation />
+						{location}
+					</div>
+					{link && (
+						<div className="flex gap-xxs items-center">
+							<IconLink />
+							<NewTabAnchor className="text-sm underline" href={link}>
+								{handleLinkText(link)}
+							</NewTabAnchor>
+						</div>
+					)}
+				</div>
+			</div>
+		</ModalTemplate>
+	)
+}
+
+function handleLinkText(link: string) {
+	if (link.includes('instagram')) {
+		return 'instagram'
+	}
+	return link
 }
