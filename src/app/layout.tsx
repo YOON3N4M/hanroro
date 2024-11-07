231f177624fdfae0279ac9a2ce18535b03b569aa
윤세남 <lgmysn2@gmail.com>
2024-11-07 09:27:46 +0900

    feat: google search (robots.txt, sitemap)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,63 +1,71 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
-import ModalPortal from "@/components/modal/ModalPortal";
-import Toolbar from "./Toolbar";
-import localFont from "next/font/local";
-import { cn } from "@/utils";
-import { API_BASE_URL } from "@/services";
-import AuthProvider from "@/components/auth/AuthProvider";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
+import ModalPortal from '@/components/modal/ModalPortal'
+import Toolbar from './Toolbar'
+import localFont from 'next/font/local'
+import { cn } from '@/utils'
+import { API_BASE_URL } from '@/services'
+import AuthProvider from '@/components/auth/AuthProvider'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS as string
+const GOOGLE_SEARCH_VERIFICATION = process.env
+	.GOOGLE_SEARCH_VERIFICATION as string
 
 export const metadata: Metadata = {
-  title: "hanroro | fanpage",
-  description: "한로로 팬페이지",
-  icons: {
-    icon: "/favicon.svg",
-  },
-  metadataBase: new URL(API_BASE_URL),
-  openGraph: {
-    images: [
-      {
-        url: "/images/profile/profile.jpg",
-        alt: "한로로",
-      },
-    ],
-  },
-};
+	title: 'hanroro | fanpage',
+	description: '한로로 팬페이지',
+	icons: {
+		icon: '/favicon.svg',
+	},
+	metadataBase: new URL(API_BASE_URL),
+	openGraph: {
+		images: [
+			{
+				url: '/images/profile/profile.jpg',
+				alt: '한로로',
+			},
+		],
+	},
+	verification: {
+		google: GOOGLE_SEARCH_VERIFICATION,
+	},
+}
 
 const pretendard = localFont({
-  src: "../../public/fonts/PretendardVariable.woff2",
-  display: "swap",
-  weight: "45 920",
-  variable: "--font-pretendard",
-});
-
-const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS as string;
+	src: '../../public/fonts/PretendardVariable.woff2',
+	display: 'swap',
+	weight: '45 920',
+	variable: '--font-pretendard',
+})
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html className="h-full" lang="en">
-      <body className={cn("h-full w-full text-authentic-dark", `${pretendard.variable}`)}>
-        <AuthProvider>
-          <Header />
-          <main className="inner h-full bg-white flex flex-col">
-            {children}
-            <Footer />
-          </main>
-          <ModalPortal />
-          {API_BASE_URL?.includes("localhost") && <Toolbar />}
-        </AuthProvider>
-      </body>
-      <GoogleAnalytics gaId={GOOGLE_ANALYTICS_ID} />
-    </html>
-  );
+	return (
+		<html className="h-full" lang="en">
+			<body
+				className={cn(
+					'h-full w-full text-authentic-dark',
+					`${pretendard.variable}`,
+				)}
+			>
+				<AuthProvider>
+					<Header />
+					<main className="inner h-full bg-white flex flex-col">
+						{children}
+						<Footer />
+					</main>
+					<ModalPortal />
+					{API_BASE_URL?.includes('localhost') && <Toolbar />}
+				</AuthProvider>
+			</body>
+			<GoogleAnalytics gaId={GOOGLE_ANALYTICS_ID} />
+		</html>
+	)
 }
---
+++ src/app/sitemap.ts
@@ -0,0 +1,10 @@
+export default async function sitemap() {
+	const baseUrl = 'https://hanroro.vercel.app/'
+
+	return [
+		{ url: baseUrl, lastModified: new Date() },
+		{ url: `${baseUrl}/profile`, lastModified: new Date() },
+		{ url: `${baseUrl}/gallery`, lastModified: new Date() },
+		{ url: `${baseUrl}/about`, lastModified: new Date() },
+	]
+}
