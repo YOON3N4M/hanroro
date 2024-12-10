2428c94b52b40ca135a0671fc2b5b98cd7880af0
yoon3nam <lgmysn2@gmail.com>
2024-12-10 10:41:06 +0900

    feat: 먹이사슬 앨범 페이지/컨테이너

---
+++ src/app/album/systemerror/page.tsx
@@ -0,0 +1,11 @@
+import SystemErrorContainer from "@/containers/Album/systemerror";
+
+interface SystemErrorPageProps {}
+
+function SystemErrorPage(props: SystemErrorPageProps) {
+  const {} = props;
+
+  return <SystemErrorContainer />;
+}
+
+export default SystemErrorPage;
