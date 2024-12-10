c4df1200dd551cd3d797fdd4a6a34576f7813265
yoon3nam <lgmysn2@gmail.com>
2024-12-10 09:39:03 +0900

    feat: 거울 앨범 페이지/컨테이너

---
+++ src/app/album/mirror/page.tsx
@@ -0,0 +1,11 @@
+import MirrorContainer from "@/containers/Album/mirror";
+
+interface MirroPageProps {}
+
+function MirroPage(props: MirroPageProps) {
+  const {} = props;
+
+  return <MirrorContainer />;
+}
+
+export default MirroPage;
