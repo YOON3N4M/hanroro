b2a3757b427d7a762534da3eaf1b71f651f176b3
yoon3nam <lgmysn2@gmail.com>
2024-12-10 09:22:39 +0900

    feat: 입춘 앨범 페이지/컨테이너

--- public/images/album/index.ts
+++ public/images/album/index.ts
@@ -5,7 +5,7 @@ export { default as evenIfYouLeave } from "./even-if-you-leave.webp";
 export { default as home } from "./home.webp";
 export { default as letMeLoveMyYouth } from "./let-me-love-my-youth.webp";
 export { default as mayfly } from "./mayfly.webp";
-export { default as mirror } from "./mirror.webp";
+export { default as mirror } from "./mirror.jpg";
 export { default as systemError } from "./system-error.webp";
 export { default as theLastStopOfPain } from "./the_last_stop_of_our_pain.webp";
 export { default as theCompass } from "./theCompass.webp";
---
+++ src/app/album/letmelovemyyouth/page.tsx
@@ -0,0 +1,11 @@
+import LetMeLoveMyYouthContainer from "@/containers/Album/letmelovemyyouth";
+
+interface LetMeLoveMyYouthPageProps {}
+
+function LetMeLoveMyYouthPage(props: LetMeLoveMyYouthPageProps) {
+  const {} = props;
+
+  return <LetMeLoveMyYouthContainer />;
+}
+
+export default LetMeLoveMyYouthPage;
