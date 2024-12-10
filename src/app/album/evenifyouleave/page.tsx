bc8deb46f2cb904f857531d7058bb3bff58890d4
yoon3nam <lgmysn2@gmail.com>
2024-12-10 10:05:29 +0900

    feat: 자처 앨범 페이지/컨테이너

---
+++ src/app/album/evenifyouleave/page.tsx
@@ -0,0 +1,11 @@
+import EvenIfYouLeaveContainer from "@/containers/Album/evenifyouleave";
+
+interface EvenIfYouLeavePageProps {}
+
+function EvenIfYouLeavePage(props: EvenIfYouLeavePageProps) {
+  const {} = props;
+
+  return <EvenIfYouLeaveContainer />;
+}
+
+export default EvenIfYouLeavePage;
