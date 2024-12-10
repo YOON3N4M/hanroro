c9997d7cc6452147dafcb94958bcd8b0af329fe3
yoon3nam <lgmysn2@gmail.com>
2024-12-10 09:47:30 +0900

    feat: 비비짝 앨범 페이지/컨테이너

---
+++ src/app/album/dontbeafraidtofall/page.tsx
@@ -0,0 +1,11 @@
+import DonBeAfraidToFallContainer from "@/containers/Album/dontbeafraidtofall";
+
+interface DonBeAfraidToFallPageProps {}
+
+function DonBeAfraidToFallPage(props: DonBeAfraidToFallPageProps) {
+  const {} = props;
+
+  return <DonBeAfraidToFallContainer />;
+}
+
+export default DonBeAfraidToFallPage;
---
+++ src/containers/Album/dontbeafraidtofall/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { generateAlbumData } from "@/utils";
+import CategorySection from "../CategorySection";
+import ImageSection from "../ImageSection";
+import InformationSection from "../InformationSection";
+import IntroSection from "../IntroSection";
+import NextAlbumSection from "../NextAlbumSection";
+import VideoSection from "../VideoSection";
+
+interface DonBeAfraidToFallContainerProps {}
+
+function DonBeAfraidToFallContainer(props: DonBeAfraidToFallContainerProps) {
+  const {} = props;
+
+  const { album, nextAlbum } = generateAlbumData("dontBeAfraidToFall");
+  if (!album) return;
+
+  const { title, releaseDate, type, desc, trackList, cover } = album;
+  return (
+    <div>
+      <IntroSection title={title} releaseDate={releaseDate} />
+      <CategorySection format={type} label="Authentic" release={releaseDate} />
+      <VideoSection src="/video/dontbeafraidtofall.mp4" />
+      <InformationSection album={album} />
+
+      <ImageSection src={"/images/album/dontbeafraidtofall/1.webp"} />
+      <ImageSection
+        src={[
+          "/images/album/dontbeafraidtofall/2.webp",
+          "/images/album/dontbeafraidtofall/4.webp",
+        ]}
+      />
+      <ImageSection
+        src={"/images/album/dontbeafraidtofall/3.webp"}
+        imageClassName="w-[80%]"
+      />
+
+      {/* <ImageSection src={"/images/album/dontbeafraidtofall/6.jpg"} /> */}
+      <NextAlbumSection album={nextAlbum} />
+    </div>
+  );
+}
+
+export default DonBeAfraidToFallContainer;
