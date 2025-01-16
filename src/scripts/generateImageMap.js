bf92c458861ce6637706181ac0e2d75f1fd90774
yoon3nam <lgmysn2@gmail.com>
2025-01-16 10:50:09 +0900

    feat: public/images/photobook 이미지 매핑 자동화
    npm run map을 실행하면
    /scripts/generateImageMap.js를 실행하고
    해당 함수는 photobook 폴더내 폴더 명을 key로
    내부 이미지를 정보를 객체로써 값으로 매핑하여
    /data/photobookData.json에 생성함
    
    따라서 포토북 이미지 추가시 npm run map만 실행하면
    페이지 내 반영은 자동으로 이루어짐

---
+++ src/scripts/generateImageMap.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const path = require("path");
+
+// 맵핑할 자식 폴더가 들어있는 부모 폴더 경로
+const targetPath = path.join(__dirname, "../../public/images/photobook");
+// 결과 Json 파일이 저장될 경로
+const outputPath = path.join(__dirname, "../data/photobookData.json");
+
+const generateImageMap = () => {
+  let result = {};
+
+  const fileList = fs.readdirSync(targetPath, { withFileTypes: true });
+
+  const folderNameList = fileList
+    .filter((item) => item.isDirectory())
+    .map((folder) => folder.name);
+
+  folderNameList.forEach((photobookPath) => {
+    const imageDir = path.join(
+      __dirname,
+      `../../public/images/photobook/${photobookPath}`
+    );
+    const files = fs.readdirSync(imageDir);
+    const data = files
+      .filter((item) => !item.includes(".DS_Store"))
+      .map((file) => {
+        return {
+          src: `/images/${photobookPath}/${file}`,
+          ref: getReference(file),
+        };
+      });
+
+    result[photobookPath] = data;
+  });
+
+  // json ㅅ앵성
+  fs.writeFileSync(outputPath, JSON.stringify(result, null, 2));
+};
+
+generateImageMap();
+
+// 파일명에서 이미지 제공자 추출
+function getReference(fileName) {
+  let ref = null;
+
+  if (fileName.includes("ref")) {
+    const splitRef = fileName.split("ref-");
+
+    const splitNumbering = splitRef[1].split("-");
+    ref = splitNumbering[0];
+  }
+
+  return ref;
+}
