07f6f80afcc35425eca4e45a7f81b15e2324eef2
yoon3nam <lgmysn2@gmail.com>
2025-01-16 13:32:19 +0900

    chore: 포토북 사이트맵 추가

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,19 @@
+import { PHOTOBOOK_LIST } from "@/containers/photobook/_local";
 import { ALBUM_LIST } from "@/data/album";
 
 const baseUrl = "https://hanroro.vercel.app";
+
+export default async function sitemap() {
+  return [
+    { url: baseUrl, lastModified: new Date() },
+    { url: `${baseUrl}/profile`, lastModified: new Date() },
+    { url: `${baseUrl}/gallery`, lastModified: new Date() },
+    { url: `${baseUrl}/about`, lastModified: new Date() },
+    { url: `${baseUrl}/calendar`, lastModified: new Date() },
+    ...generateAlbumSiteMap(),
+    ...generatePhotobookSiteMap(),
+  ];
+}
 
 function generateAlbumSiteMap() {
   const albumSiteMap = ALBUM_LIST.map((album) => ({
@@ -11,13 +24,11 @@ function generateAlbumSiteMap() {
   return albumSiteMap;
 }
 
-export default async function sitemap() {
-  return [
-    { url: baseUrl, lastModified: new Date() },
-    { url: `${baseUrl}/profile`, lastModified: new Date() },
-    { url: `${baseUrl}/gallery`, lastModified: new Date() },
-    { url: `${baseUrl}/about`, lastModified: new Date() },
-    { url: `${baseUrl}/calendar`, lastModified: new Date() },
-    ...generateAlbumSiteMap(),
-  ];
+function generatePhotobookSiteMap() {
+  const photobookSiteMap = PHOTOBOOK_LIST.map((photobook) => ({
+    url: `${baseUrl}/album/${photobook.titleEng.toLowerCase()}`,
+    lastModified: new Date(),
+  }));
+
+  return photobookSiteMap;
 }
