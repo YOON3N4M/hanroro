eb27df26db60e64b5d7f27e0e4f4901951ed81c6
yoon3nam <lgmysn2@gmail.com>
2025-01-23 16:50:22 +0900

    feat: 춘베리아 포토북

--- src/containers/photobook/_local/index.tsx
+++ src/containers/photobook/_local/index.tsx
@@ -19,29 +19,31 @@ export interface PhotobookImageData {
 
 const GERMINATION: Photobook = {
   title: "HANRORO 3rd CONCERT ‘발아(發芽)’",
-  // type: "concert",
   titleEng: "germination",
   imageList: photobookJson.germination,
 };
 
 const HYUNDAI_DIVE: Photobook = {
   title: "현대카드 Curated 95",
-  // type: "concert",
   titleEng: "hyundai-card-dive",
   imageList: photobookJson["hyundai-card-dive"],
-  // thumbnail: "2.JPG",
 };
 
 const HYPEBEAST: Photobook = {
   title: "Hypebeast 인터뷰",
-  // type: "concert",
   titleEng: "hypebeast",
-  imageList: photobookJson["hypebeast"],
-  // thumbnail: "2.JPG",
+  imageList: photobookJson.hypebeast,
+};
+
+const CHUNBEIRA: Photobook = {
+  title: "2024 춘베리아 특급열차",
+  titleEng: "chunberia",
+  imageList: photobookJson.chunberia,
 };
 
 export const PHOTOBOOK_LIST: Photobook[] = [
   GERMINATION,
+  CHUNBEIRA,
   HYUNDAI_DIVE,
   HYPEBEAST,
 ];
