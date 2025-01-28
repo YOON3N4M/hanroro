71625fa98f5c396468e2004ff3d9cc879b5c3fe0
yoon3nam <lgmysn2@gmail.com>
2025-01-28 12:31:48 +0900

    feat: 포토북 컨텐츠 업데이트
    arenakorea, hatchingroom, umbro, chunberia,thanksgiving

--- src/containers/photobook/_local/index.tsx
+++ src/containers/photobook/_local/index.tsx
@@ -41,9 +41,44 @@ const CHUNBEIRA: Photobook = {
   imageList: photobookJson.chunberia,
 };
 
+const HATCHING_ROOM: Photobook = {
+  title: "한로로 X 해칭룸",
+  titleEng: "hatchingroom",
+  imageList: photobookJson.hatchingroom,
+};
+
+const UMBRO: Photobook = {
+  title: "한로로 X 엄브로",
+  titleEng: "umbro",
+  imageList: photobookJson.umbro,
+};
+
+const ARENA_KOREA: Photobook = {
+  title: "아레나 코리아 화보",
+  titleEng: "arenakorea",
+  imageList: photobookJson.arenakorea,
+};
+
+const THANKSGIVING: Photobook = {
+  title: "2024 추석 화보",
+  titleEng: "thanksgiving",
+  imageList: photobookJson.thanksgiving,
+};
+
+// const SEBS: Photobook = {
+//   title: "2024 SEBS",
+//   titleEng: "sebs",
+//   imageList: photobookJson.sebs,
+// };
+
 export const PHOTOBOOK_LIST: Photobook[] = [
   GERMINATION,
   CHUNBEIRA,
   HYUNDAI_DIVE,
   HYPEBEAST,
+  HATCHING_ROOM,
+  UMBRO,
+  ARENA_KOREA,
+  THANKSGIVING,
+  // SEBS,
 ];
