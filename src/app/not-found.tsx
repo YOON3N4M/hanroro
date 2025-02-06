c02369b81bd76fc7959ec7673802587d7dab635a
yoon3nam <lgmysn2@gmail.com>
2025-02-06 17:00:32 +0900

    fix: not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,11 +3,13 @@ import React from "react";
 
 export default function NotFoundPage() {
   return (
-    <div className="my-auto text-xs inner">
-      <p>존재하지 않는 페이지 입니다.</p>
-      <Link href={"/"} className="underline">
-        메인으로 이동
-      </Link>
+    <div className="my-auto text-xs inner y-inner min-h-screen relative">
+      <div className="absolute x-center top-1/4">
+        <p>존재하지 않는 페이지 입니다.</p>
+        <Link href={"/"} className="underline">
+          메인으로 이동
+        </Link>
+      </div>
     </div>
   );
 }
