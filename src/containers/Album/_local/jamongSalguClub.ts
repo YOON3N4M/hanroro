f49268f5bc3f0c853b0d7e453572fd274273909e
yoon3n4m <lgmysn2@gmail.com>
2025-11-19 15:31:03 +0900

    fix: add jamong salgu club image and video

--- src/containers/Album/_local/jamongSalguClub.ts
+++ src/containers/Album/_local/jamongSalguClub.ts
@@ -3,12 +3,12 @@ import { LocalAlbumPageData } from ".";
 
 export const JAMONG_SALGU_CLUB_PAGE_DATA: LocalAlbumPageData = {
   album: ALBUM_JAMONG_SALGU_CLUB,
-  videoSection: "/video/home.mp4",
+  videoSection: "/video/jamongsalguclub.mp4",
   imageSectionList: [
-    ["/images/album/home/1.webp", "/images/album/home/2.webp"],
-    ["/images/album/home/3.webp", "/images/album/home/4.webp"],
-    ["/images/album/home/5.webp"],
-    ["/images/album/home/6.webp", "/images/album/home/7.jpg"],
+    ["/images/album/jamongsalguclub/1.webp"],
+    // ["/images/album/jamongsalguclub/3.webp", "/images/album/jamongsalguclub/4.webp"],
+    // ["/images/album/jamongsalguclub/5.webp"],
+    // ["/images/album/jamongsalguclub/6.webp", "/images/album/jamongsalguclub/7.jpg"],
   ],
-  keyColor: "#bf06258a",
+  keyColor: "#91C18A",
 };
